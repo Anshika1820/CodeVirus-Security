b6a394cf8641c7a49300a7897e683c3da56cb3c0
Anshika Srivastava <srivastavaanshika510@gmail.com>
2026-02-26 18:31:25 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -51,7 +51,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex items-center justify-between w-full">
+    
     <header
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
         scrolled
@@ -195,7 +195,7 @@ const Navbar = () => {
     
     )}
     </header>
-    </div>
+    
   );
 };
 
